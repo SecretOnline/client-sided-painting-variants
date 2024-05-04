f5419e076e6c68b0106318ebf9b23cfe780e1393
secret_online <me@secretonline.co>
2024-05-04 12:50:07 +1200

    Initialise project from fabric starter

---
+++ src/client/java/co/secretonline/morepaintingsontheclient/MorePaintingsOnTheClientClient.java
@@ -0,0 +1,10 @@
+package co.secretonline.morepaintingsontheclient;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MorePaintingsOnTheClientClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
