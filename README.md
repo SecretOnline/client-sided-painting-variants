97bf5e71302f789eff73c2c08fb9e78ff493ba71
secret_online <me@secretonline.co>
2024-05-04 19:50:24 +1200

    Read painting variant files from resource packs

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # more-paintings-on-the-client
+
+## For resource/data pack developers
+
+Players with this mod installed can have your paintings added into the pool of available paintings wherever they go with your resource pack, and not just on servers with the data pack. Since this mod doesn't remove any of Minecraft's default paintings, it's a nice non-destructive way to have your artwork integrated into the game.
+
+In order to make your resource pack work, all you need to do is copy the `data/<namespace>/painting_variants/` folder with all the JSON files from your _data_ pack, and move it to `assets/<namespace>/painting_variants/` in your _resource_ pack. Done!
+
+I'd love to make this a no-effort thing for pack developers (which would also have the benefit of working out-of-the-box), but unfortunately the resource/data pack split makes the ideal version of this project impossible. Instead, I think this is the next best thing.
--- src/client/java/co/secretonline/morepaintingsontheclient/MorePaintingsResourceListener.java
+++ src/client/java/co/secretonline/morepaintingsontheclient/MorePaintingsResourceListener.java
@@ -1,5 +1,6 @@
 package co.secretonline.morepaintingsontheclient;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -12,9 +13,11 @@ import net.minecraft.entity.decoration.painting.PaintingVariant;
 import net.minecraft.registry.Registries;
 import net.minecraft.resource.ResourceManager;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.JsonHelper;
 import net.minecraft.util.profiler.Profiler;
 
 public class MorePaintingsResourceListener implements IdentifiableResourceReloadListener {
+	private static final int NUM_PIXELS_PER_BLOCK = 16;
 
 	@Override
 	public CompletableFuture<Void> reload(Synchronizer synchronizer, ResourceManager resourceManager,
@@ -32,46 +35,60 @@ public class MorePaintingsResourceListener implements IdentifiableResourceReload
 		// This also means that if a server provides a painting that is also provided by
 		// the client, we can de-duplicate later.
 		var paintingsFromRegistryFuture = CompletableFuture.supplyAsync(() -> {
-			Map<String, Map<Identifier, PaintingVariant>> paintingSizes = new HashMap<>();
+			Map<Identifier, PaintingVariant> paintings = new HashMap<>();
 
 			Registries.PAINTING_VARIANT.forEach((painting) -> {
-				var size = painting.getWidth() + "x" + painting.getHeight();
-
-				if (!paintingSizes.containsKey(size)) {
-					paintingSizes.put(size, new HashMap<>());
-				}
-				var paintingsForSize = paintingSizes.get(size);
-
-				paintingsForSize.put(Registries.PAINTING_VARIANT.getId(painting), painting);
+				paintings.put(Registries.PAINTING_VARIANT.getId(painting), painting);
 			});
 
-			return paintingSizes;
+			return paintings;
 		}, prepareExecutor);
 
-		// When there are more steps to do in the propare stage, will need to combine
-		// these somehow.
-		// I kind of miss the flattenning that Javascript/Typescript does with Promises.
-		// A Promise<Promise<T>> is flattened down to just Promise<T>. For now this
-		// assignment will just live in peace, taking up space on the stack for no
-		// reason.
-		// var prepareStage = CompletableFuture.supplyAsync(() -> {
-		// return getPaintingsFromRegistry;
-		// }, prepareExecutor);
-		var prepareStage = paintingsFromRegistryFuture;
+		var paintingsFromResourcesFuture = CompletableFuture.supplyAsync(() -> {
+			Map<Identifier, PaintingVariant> paintings = new HashMap<>();
+
+			var allVariantJsonFiles = resourceManager.findResources("painting_variant",
+					identifier -> identifier.getPath().endsWith(".json"));
+
+			allVariantJsonFiles.forEach((identifier, resource) -> {
+				try (var reader = resource.getReader()) {
+					var data = JsonHelper.deserialize(reader).getAsJsonObject();
+					var width = data.get("width").getAsInt();
+					var height = data.get("height").getAsInt();
+					// TODO: Somehow get texture information through the process
+					// var assetId = data.get("asset_id").getasString();
+
+					paintings.put(identifier, new PaintingVariant(width * NUM_PIXELS_PER_BLOCK, height * NUM_PIXELS_PER_BLOCK));
+				} catch (IOException ex) {
+					MorePaintingsOnTheClient.LOGGER.info("Failed to read data for " + identifier.toString() + ". Skipping");
+				}
+			});
+
+			return paintings;
+		}, prepareExecutor);
+
+		// Combine steps from prepare stage
+		var prepareStage = paintingsFromRegistryFuture.thenCombine(paintingsFromResourcesFuture,
+				(fromRegistry, fromResources) -> {
+					var basket = new PaintingsBasket();
+					basket.registryPaintings = fromRegistry;
+					basket.resourcesPaintings = fromResources;
+
+					return basket;
+				});
 
 		var afterSync = prepareStage.thenCompose(synchronizer::whenPrepared);
 
-		var applyStage = afterSync.thenAcceptAsync((minecraftPaintings) -> {
+		var applyStage = afterSync.thenAcceptAsync((basket) -> {
 			MorePaintingsOnTheClient.LOGGER.info("Yeah paintings! Woo!");
 
-			minecraftPaintings.forEach((size, variants) -> {
-				List<String> ids = new ArrayList<>();
-				variants.forEach((id, variant) -> {
-					ids.add(id.toString());
-				});
+			List<String> registeredIds = new ArrayList<>();
+			basket.registryPaintings.forEach((id, variant) -> registeredIds.add(id.toString()));
+			MorePaintingsOnTheClient.LOGGER.info("Registered: " + String.join(", ", registeredIds));
 
-				MorePaintingsOnTheClient.LOGGER.info(size + ": " + String.join(", ", ids));
-			});
+			List<String> ids = new ArrayList<>();
+			basket.resourcesPaintings.forEach((id, variant) -> ids.add(id.toString()));
+			MorePaintingsOnTheClient.LOGGER.info("Client only: " + String.join(", ", ids));
 		}, applyExecutor);
 
 		return applyStage;
@@ -82,4 +99,11 @@ public class MorePaintingsResourceListener implements IdentifiableResourceReload
 		return new Identifier(MorePaintingsOnTheClient.MOD_ID, "resource-listener");
 	}
 
+	private class PaintingsBasket {
+		// TODO: Figure out what info is actually needed
+
+		Map<Identifier, PaintingVariant> registryPaintings;
+
+		Map<Identifier, PaintingVariant> resourcesPaintings;
+	}
 }
