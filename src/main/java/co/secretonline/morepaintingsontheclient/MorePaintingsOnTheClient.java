72b9ed51a18dcfe9c12573f28aa21273a0adb423
secret_online <me@secretonline.co>
2024-05-04 14:01:08 +1200

    Add .editorconfig
    The scaffolded  project uses tabs, so I guess we're using tabs.

--- src/main/java/co/secretonline/morepaintingsontheclient/MorePaintingsOnTheClient.java
+++ src/main/java/co/secretonline/morepaintingsontheclient/MorePaintingsOnTheClient.java
@@ -9,7 +9,7 @@ public class MorePaintingsOnTheClient implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-    public static final Logger LOGGER = LoggerFactory.getLogger("more-paintings-on-the-client");
+	public static final Logger LOGGER = LoggerFactory.getLogger("more-paintings-on-the-client");
 
 	@Override
 	public void onInitialize() {
