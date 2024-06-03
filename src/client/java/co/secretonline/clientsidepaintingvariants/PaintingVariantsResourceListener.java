f00b0e5964f51db687fbcd732987a1e0cafbffbb
secret_online <me@secretonline.co>
2024-06-03 12:31:31 +1200

    Small refactor of resource loader

--- src/client/java/co/secretonline/clientsidepaintingvariants/PaintingVariantsResourceListener.java
+++ src/client/java/co/secretonline/clientsidepaintingvariants/PaintingVariantsResourceListener.java
@@ -31,72 +31,82 @@ public class PaintingVariantsResourceListener implements IdentifiableResourceRel
 			Executor prepareExecutor, Executor applyExecutor) {
 		// Rough plan for this method:
 		// In the prepare stage, read paintings from the registry and resource packs.
-		// In the apply stage, do some stuff. This is likely to include stiching a new
-		// texture atlas but since I don't actually now how this works I'm really just
-		// guessing here.
+		// In the apply stage, store statically for the mixin to pick up.
 
-		// 1.21 makes paintings data-driven, so we need to figure out how many paintings
-		// Minecraft provides every time.
-		// This also means that if a server provides a painting that is also provided by
-		// the client, we can de-duplicate later.
-		var paintingsFromRegistryFuture = CompletableFuture.supplyAsync(() -> {
-			Map<Identifier, PaintingVariant> paintings = new HashMap<>();
+		var getRegistry = CompletableFuture.supplyAsync(
+				() -> this.getPaintingsFromRegistry(),
+				prepareExecutor);
 
-			var paintingRegistryWrapper = BuiltinRegistries.createWrapperLookup()
-					.getWrapperOrThrow(RegistryKeys.PAINTING_VARIANT);
+		var getResources = CompletableFuture.supplyAsync(
+				() -> this.getPaintingsFromResources(resourceManager),
+				prepareExecutor);
 
-			paintingRegistryWrapper.streamEntries().forEach((painting) -> {
-				paintings.put(painting.getKey().orElseThrow().getValue(), painting.value());
-			});
-
-			return paintings;
-		}, prepareExecutor);
-
-		var paintingsFromResourcesFuture = CompletableFuture.supplyAsync(() -> {
-			Map<Identifier, AddedPaintingVariant> paintings = new HashMap<>();
-
-			var allVariantJsonFiles = resourceManager.findResources("painting_variant",
-					identifier -> identifier.getPath().endsWith(".json"));
-
-			allVariantJsonFiles.forEach((identifier, resource) -> {
-				try (var reader = resource.getReader()) {
-					var data = JsonHelper.deserialize(reader).getAsJsonObject();
-					var width = data.get("width").getAsInt();
-					var height = data.get("height").getAsInt();
-
-					var assetId = Identifier.of(data.get("asset_id").getAsString());
-
-					paintings.put(identifier, new AddedPaintingVariant(width, height, assetId));
-				} catch (IOException ex) {
-					LOGGER.warn("Failed to read data for " + identifier.toString() + ". Skipping");
-				} catch (Exception ex) {
-					LOGGER.warn("Error while reading painting variant " + identifier.toString() + ". Skipping");
-				}
-			});
-
-			return paintings;
-		}, prepareExecutor);
-
-		// Combine steps from prepare stage
-		var prepareStage = paintingsFromResourcesFuture;
-
-		var afterSync = prepareStage.thenCompose(synchronizer::whenPrepared);
-
-		var applyStage = afterSync
-				.thenCombine(paintingsFromRegistryFuture, (fromResources, fromRegistry) -> {
-					var info = new PaintingVariantsInfo();
-
-					fromRegistry.forEach((identifier, variant) -> info.addRegisteredPainting(identifier, variant));
-					fromResources.forEach((identifier, variant) -> info.addAddedPainting(identifier, variant));
-
-					return info;
-				})
+		CompletableFuture<Void> future = getResources
+				.thenCombine(
+						getRegistry,
+						(fromResources, fromRegistry) -> this.combineVariantInfo(fromRegistry, fromResources))
+				.thenCompose(synchronizer::whenPrepared)
 				.thenAcceptAsync((info) -> {
 					LOGGER.info(info.getSummaryString());
 					KNOWN_PAINTINGS = info;
 				}, applyExecutor);
 
-		return applyStage;
+		return future;
+	}
+
+	private Map<Identifier, PaintingVariant> getPaintingsFromRegistry() {
+		Map<Identifier, PaintingVariant> paintings = new HashMap<>();
+
+		// 1.21 puts paintings in a data-driven registry, which makes this a little more
+		// complicated.
+		// I don't know whether this is the best way to go about this, but it works so
+		// Im happy enough.
+		var paintingRegistryWrapper = BuiltinRegistries.createWrapperLookup()
+				.getWrapperOrThrow(RegistryKeys.PAINTING_VARIANT);
+
+		paintingRegistryWrapper.streamEntries().forEach((painting) -> {
+			paintings.put(painting.getKey().orElseThrow().getValue(), painting.value());
+		});
+
+		return paintings;
+	}
+
+	private Map<Identifier, AddedPaintingVariant> getPaintingsFromResources(ResourceManager resourceManager) {
+		Map<Identifier, AddedPaintingVariant> paintings = new HashMap<>();
+
+		// Load all files from resource packs that should contain painting variants.
+		// Vanilla paintings shouldn't appear in this list, as this is reading from
+		// resources and not data.
+		var allVariantJsonFiles = resourceManager.findResources("painting_variant",
+				identifier -> identifier.getPath().endsWith(".json"));
+
+		allVariantJsonFiles.forEach((identifier, resource) -> {
+			try (var reader = resource.getReader()) {
+				var data = JsonHelper.deserialize(reader).getAsJsonObject();
+				var width = data.get("width").getAsInt();
+				var height = data.get("height").getAsInt();
+
+				var assetId = Identifier.of(data.get("asset_id").getAsString());
+
+				paintings.put(identifier, new AddedPaintingVariant(width, height, assetId));
+			} catch (IOException ex) {
+				LOGGER.warn("Failed to read data for " + identifier.toString() + ". Skipping");
+			} catch (Exception ex) {
+				LOGGER.warn("Error while reading painting variant " + identifier.toString() + ". Skipping");
+			}
+		});
+
+		return paintings;
+	}
+
+	private PaintingVariantsInfo combineVariantInfo(Map<Identifier, PaintingVariant> registryPaintings,
+			Map<Identifier, AddedPaintingVariant> addedPaintings) {
+		var info = new PaintingVariantsInfo();
+
+		registryPaintings.forEach((identifier, variant) -> info.addRegisteredPainting(identifier, variant));
+		addedPaintings.forEach((identifier, variant) -> info.addAddedPainting(identifier, variant));
+
+		return info;
 	}
 
 	@Override
